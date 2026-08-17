f494956ef830c67094abd2a13298df1ce0013516
luismaradiaga28 <luis.maradiaga5050@gmail.com>
2026-08-17 11:26:42 -0600

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,2 +1,12 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Luis - La trazabilidad y el control de versiones permiten gestionar cambios y facilitan el trabajo colaborativo.");
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Mi nombre es: Luis Alberto Maradiaga");
+        Console.WriteLine("Mi principio Agil: hacer un buen trabajo y cumplir todo lo que se me proponga");
+    }
+}
