293bed97e2537319225768bf5e976972a1102ad3
abelMorales12 <abelmoraless125@gmail.com>
2026-08-16 21:58:53 -0600

    Create Program.cs

---
+++ docs/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integrante: Abel Morales - Los 4 Valores y 12 Principios del Manifiesto Ágil");
+        }
+    }
+}
